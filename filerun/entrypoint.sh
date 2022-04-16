9d0ef9200156e01ea09f052d1831994606068c3e
Karthik Anantharamu <akarthik101@gmail.com>
2022-04-16 02:11:42 -0400

    Create entrypoint.sh

---
+++ filerun/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eux
+
+# Check if user exists
+if ! id -u ${APACHE_RUN_USER} > /dev/null 2>&1; then
+	echo "The user ${APACHE_RUN_USER} does not exist, creating..."
+	groupadd -f -g ${APACHE_RUN_GROUP_ID} ${APACHE_RUN_GROUP}
+	useradd -u ${APACHE_RUN_USER_ID} -g ${APACHE_RUN_GROUP} ${APACHE_RUN_USER}
+fi
+
+exec "$@"
